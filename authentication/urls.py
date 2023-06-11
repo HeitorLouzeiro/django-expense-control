f18055dafea1a3db111fa757c2f109989238689f
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-06-11 16:28:44 -0300

    template register with user validation ajax.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
-from .views import RegistrationView
+from .views import RegistrationView, UsernameValidation
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
-
+    path('registration/validate-username/', csrf_exempt(UsernameValidation.as_view()),
+         name='validate-username'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,25 @@
+import json
+
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
+
+
+class UsernameValidation(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'}, status=400)  # noqa
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Username is already taken'}, status=409)  # noqa
+
+        return JsonResponse({'username_valid': True})
 
 
 class RegistrationView(View):
