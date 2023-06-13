dace29d579fdbc44c3c0c9b960ea62f87de405e8
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-06-13 11:37:32 -0300

    Account activation by email.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import (EmailValidationView, RegistrationView,
-                    UsernameValidationView)
+from .views import (EmailValidationView, LoginView, RegistrationView,
+                    UsernameValidationView, VerificationView)
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login'),
     path('registration/validate-username/', csrf_exempt(UsernameValidationView.as_view()),
          name='validate-username'),
 
     path('registration/validate-email/', csrf_exempt(EmailValidationView.as_view()),
          name='validate-email'),
+    path('registration/activate/<uidb64>/<token>/',
+         VerificationView.as_view(), name='activate'),
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,10 +2,17 @@ import json
 
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.views import View
 from validate_email import validate_email
+
+from .utils import token_generator
 
 # Create your views here.
 
@@ -62,12 +69,62 @@ class RegistrationView(View):
                 # Create user account
                 user = User.objects.create_user(username=username, email=email)
                 user.set_password(password)
+                user.is_active = False
                 user.save()
+
+                domain = get_current_site(request).domain
+                uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+                link = reverse('authentication:activate', kwargs={
+                    'uidb64': uidb64, 'token': token_generator.make_token(user)
+                })
+
+                activate_url = 'http://' + domain + link
+
+                email_subject = 'Activate your account'
+                email_body = 'Hi ' + user.username + ' Please use this link to verify your account\n' + activate_url  # noqa
+
+                email = EmailMessage(
+                    email_subject,
+                    email_body,
+                    'norepley@seycolon.com',
+                    [email],
+
+                )
+                email.send(fail_silently=False)
                 messages.success(request, 'Account created successfully')
                 return render(request, 'authentication/pages/register.html')
             else:
                 messages.error(request, 'Email is already taken')
                 return render(request, 'authentication/pages/register.html')
 
-        messages.success(request, 'Account created successfully')
+        messages.success(
+            request, 'Account created successfully, please check your email box for account activation.')  # noqa
         return render(request, 'authentication/pages/register.html')
+
+
+class VerificationView(View):
+    def get(self, request, uidb64, token):
+        try:
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=id)
+
+            if not token_generator.check_token(user, token):
+                return redirect('login'+'?message='+'User already activated')
+
+            if user.is_active:
+                return redirect('login')
+            user.is_active = True
+            user.save()
+
+            messages.success(request, 'Account activated successfully')
+            return redirect('login')
+
+        except Exception as ex:
+            pass
+
+        return redirect('authentication:login')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'authentication/pages/login.html')
