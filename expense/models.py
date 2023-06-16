eda8290f1e856ce98b6baed57af3407dce5f7b4f
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-06-16 12:32:35 -0300

    Added crud add expense.

--- expense/models.py
+++ expense/models.py
@@ -1,3 +1,28 @@
+# Create your models here.
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Expense(models.Model):
+    amount = models.FloatField()
+    description = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description + ' - ' + str(self.amount)
+
+    class Meta:
+        ordering = ['-date']
--- expense/views.py
+++ expense/views.py
@@ -1,14 +1,53 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .models import Category, Expense
 
 # Create your views here.
 
 
 @login_required(login_url='authentication:login', redirect_field_name='next')
 def home(request):
-    return render(request, 'expense/pages/home.html')
+    categories = Category.objects.filter(user=request.user)
+    expenses = Expense.objects.filter(user=request.user)
+    context = {
+        'categories': categories,
+        'expenses': expenses,
+    }
+
+    return render(request, 'expense/pages/home.html', context)
 
 
 @login_required(login_url='authentication:login', redirect_field_name='next')
 def addExpense(request):
-    return render(request, 'expense/pages/addExpense.html')
+    template_name = 'expense/pages/addExpense.html'
+    categories = Category.objects.filter(user=request.user)
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, template_name, context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, template_name, context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        category = Category.objects.get(id=category)
+
+        if not description:
+            messages.error(request, 'description is required')
+            return render(request, template_name, context)
+
+        Expense.objects.create(user=request.user, amount=amount, date=date,
+                               category=category, description=description)
+        messages.success(request, 'Expense saved successfully')
+
+        return redirect('expense:home')
